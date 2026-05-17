947d6a123ab5ebcd3e1e6b59c5fb73fdb907cdc6
kashamrokaya-cyber <kashamrokaya18@gmail.com>
2026-05-17 12:55:25 +0545

    gap

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -45,7 +45,7 @@ const Navbar = ({ darkMode, toggleDarkMode }) => {
           </Link>
 
           {/* Desktop Nav */}
-          <div className="hidden md:flex items-center gap-8">
+          <div className="hidden md:flex items-center gap-6">
             {navLinks.map((link) => (
               <NavLink
                 key={link.to}
